54e91d848bec123e1e4348bcacca0322d7f40622
lribando <lribando@hotmail.com>
2015-10-13 18:01:19 -0400

    Update bestPractices.js

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,28 +8,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.bestPracticesAnswers = {
   globals : function() {
-    myObject = {
+    var myObject = {
       name : 'Jory'
     };
-
     return myObject;
   },
 
   functions : function(flag) {
     if (flag) {
-      function getValue() { return 'a'; }
+      function getValueA() { return 'a'; }
     } else {
-      function getValue() { return 'b'; }
+      function getValueB() { return 'b'; }
     }
 
-    return getValue();
+    return getValueA();
   },
 
   parseInt : function(num) {
-    return parseInt(num);
+    return parseInt(num,10);
   },
 
   identity : function(val1, val2) {
+    if (val1 === val2) {
+      return true;
+    } else {
+      return false;
+    }
 
   }
 };
