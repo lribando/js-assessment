dd4bddd6642884401507646db8ae88e4afadc806
lribando <lribando@hotmail.com>
2015-10-13 18:02:24 -0400

    Update strings.js

--- app/strings.js
+++ app/strings.js
@@ -8,6 +8,10 @@ exports.stringsAnswers = {
 
   },
   reverseString: function(str) {
-
+    var rev = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+    return rev;
   }
 };
