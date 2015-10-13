e9d12d33f8ac94d7cc659416917ec79553fefeb7
lribando <lribando@hotmail.com>
2015-10-13 18:00:37 -0400

    Work in Progress

--- app/functions.js
+++ app/functions.js
@@ -2,15 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(this,arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.call(obj, fn);
   },
 
   functionFunction : function(str) {
-
+    var final = str;
+    function string2(string) {
+      final = str + "," + " " + string;
+      return final;
+    }
+    return string2;
   },
 
   makeClosures : function(arr, fn) {
