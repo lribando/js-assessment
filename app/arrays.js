2f814773a2b39b05683abf28e28f41ea547488f5
System Administrator <root@ml6901.home>
2015-10-13 01:35:29 -0400

    Work in progress

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,25 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    var n = arr.indexOf(item);
+    return n;
   },
 
   sum : function(arr) {
-
+    var x = 0;
+    for (var i = 0; i < arr.length; i++) {
+      x += arr[i];
+    }
+    return x;
   },
 
   remove : function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i,1)
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
@@ -19,31 +29,46 @@ exports.arraysAnswers = {
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
+    arr.unshift(item);
+    return arr;
 
   },
 
   curtail : function(arr) {
+    arr.shift();
+    return arr;
 
   },
 
   concat : function(arr1, arr2) {
+    var arr3 = arr1.concat(arr2);
+    return arr3;
 
   },
 
   insert : function(arr, item, index) {
-
+    arr += arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    var y = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        y += 1;
+      }
+    }
+    return y;
   },
 
   duplicates : function(arr) {
@@ -51,10 +76,18 @@ exports.arraysAnswers = {
   },
 
   square : function(arr) {
-
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = Math.pow(arr[i], 2);
+    }
+    return arr;
   },
 
   findAllOccurrences : function(arr, target) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        var y = i;
+      }
+    }
+    return y;
   }
 };
